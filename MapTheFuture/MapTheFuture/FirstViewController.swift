30484be7ad3ce8c9a703d17a7ebec13703f9d882
macbellingrath <macbellingrath@gmail.com>
2015-11-18 17:39:04 -0500

    Create Xcode proj

---
+++ MapTheFuture/MapTheFuture/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MapTheFuture
+//
+//  Created by Mac Bellingrath on 11/18/15.
+//  Copyright © 2015 Mac Bellingrath. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
